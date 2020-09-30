6fe1f530b99c5f20c2a71c04b730a9cf3e0e3e05
Rafael Rios <rafaelrafssk@hotmail.com>
2020-09-30 13:46:24 -0300

    create first exe tableView

---
+++ TableViewCollectionViewExercises/Exe01_TableView/Exe01_TableView/Person/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  Exe01_TableView
+//
+//  Created by Rafael Rios on 9/30/20.
+//
+
+import Foundation
+import UIKit
+
+class Person {
+    var name: String
+    var responsability: String
+    var email: String
+    
+    init(name: String, responsability: String, email: String) {
+        self.name = name
+        self.responsability = responsability
+        self.email = email
+    }
+}
+
+
