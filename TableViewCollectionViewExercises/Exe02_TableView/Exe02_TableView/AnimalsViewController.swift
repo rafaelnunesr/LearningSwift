ddd53228928f12ca89395bb6834e64c4e82f8d9b
Rafael Rios <rafaelrafssk@hotmail.com>
2020-10-01 10:36:25 -0300

    exe 02

---
+++ TableViewCollectionViewExercises/Exe02_TableView/Exe02_TableView/AnimalsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exe02_TableView
+//
+//  Created by Rafael Rios on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
