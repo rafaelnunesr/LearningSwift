2e187a009c3a935fdc71906154516b3d3b709656
Rafael Nunes Rios <rafaelrafssk@hotmail.com>
2020-10-20 12:27:14 -0300

    create datepicker

---
+++ DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by Rafael Nunes Rios on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func myDatePicker(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let strDate = dateFormatter.string(from: datePicker.date)
+        print(strDate)
+        
+        let strDateSender = dateFormatter.string(from: sender.date)
+        print(strDateSender)
+        
+        let str = datePicker.date
+        print(str)
+        
+        textLabel.text = strDateSender
+        
+    }
+}
+
