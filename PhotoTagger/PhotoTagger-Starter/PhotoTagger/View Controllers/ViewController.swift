edde1c44f040a4ff383ef193e3b166a904e950d9
Rafael Nunes Rios <rafaelrafssk@hotmail.com>
2020-11-11 16:03:07 -0300

    learning how to work with api

---
+++ PhotoTagger/PhotoTagger-Starter/PhotoTagger/View Controllers/ViewController.swift
@@ -0,0 +1,153 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet var takePictureButton: UIButton!
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var progressView: UIProgressView!
+  @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+
+  // MARK: - Properties
+  private var tags: [String]?
+  private var colors: [PhotoColor]?
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+      takePictureButton.setTitle("Select Photo", for: .normal)
+    }
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    imageView.image = nil
+  }
+
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    if segue.identifier == "ShowResults",
+      let controller = segue.destination as? TagsColorsViewController {
+      controller.tags = tags
+      controller.colors = colors
+    }
+  }
+
+  // MARK: - IBActions
+  @IBAction func takePicture(_ sender: UIButton) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = false
+
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = .camera
+    } else {
+      picker.sourceType = .photoLibrary
+      picker.modalPresentationStyle = .fullScreen
+    }
+
+    present(picker, animated: true)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      print("Info did not have the required UIImage for the Original Image")
+      dismiss(animated: true)
+      return
+    }
+
+    imageView.image = image
+
+    // Hide the upload button, and show the progress view and activity view
+    takePictureButton.isHidden = true
+    progressView.progress = 0.0
+    progressView.isHidden = false
+    activityIndicatorView.startAnimating()
+    
+    upload(image: image,
+           progressCompletion: { [weak self] percent in
+            
+            // While the file uploads, you call the progress handler with an updated percent
+            self?.progressView.setProgress(percent, animated: true)
+           },
+           completion: { [weak self] tags, colors in
+            
+            // The completion handler executes when the upload finishes. This sets the controls back to the original state
+            self?.takePictureButton.isHidden = false
+            self?.progressView.isHidden = true
+            self?.activityIndicatorView.stopAnimating()
+            
+            self?.tags = tags
+            self?.colors = colors
+            
+            // Finally the Storyboard advances to the results screen when the upload completes, successfully or not. The user interface doen't change based on the error condition
+            self?.performSegue(withIdentifier: "ShowResults", sender: self)
+           })
+    
+    dismiss(animated: true)
+  }
+}
+
+extension ViewController {
+  func upload(image: UIImage,
+              progressCompletion: @escaping (_ percent: Float) -> Void,
+              completion: @escaping (_ tags: [String]?, _ colors: [PhotoColor]?) -> Void) {
+    
+    // The image that's being uploaded needs to be converted to be a Data instance
+    guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+      print("Could not get JPEG representation for UIIMage")
+      return
+    }
+    
+    
+    // Here the convert the JPEG data blob imageData into a MIME multipart request to send to the Imagga content endpoint
+    Alamofire.upload(multipartFormData: { multipartFormData in
+      multipartFormData.append(imageData,
+                               withName: "imageFile",
+                               fileName: "image.jpg",
+                               mimeType: "image/jpeg")
+      
+    },
+    to: "http://api.imagga.com/v1/content",
+    headers: ["Authorization": "Basic xxx"],
+    encodingCompletion: { encondingResult in })
+  }
+}
